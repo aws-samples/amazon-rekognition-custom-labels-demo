b1c728f7665e37d34b22459a822439503fb2309d
Matteo Figus <matteofigus@gmail.com>
2019-11-29 18:09:11 +0000

    More error handling around API issues

--- src/web-ui/src/components/ProjectsSummary.js
+++ src/web-ui/src/components/ProjectsSummary.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Alert, Button, Card, Spinner } from "react-bootstrap";
 import { mapResults } from "../utils";
 
 export default ({ gateway, onVersionClick }) => {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState(undefined);
+  const [errorDetails, setErrorDetails] = useState(undefined);
 
   useEffect(() => {
     gateway
@@ -14,7 +15,8 @@ export default ({ gateway, onVersionClick }) => {
             gateway.describeProjectVersions(project.ProjectArn)
           )
         ).then(x => setProjects(mapResults(x)))
-      );
+      )
+      .catch(e => setErrorDetails(e.toString()));
   }, [gateway]);
 
   return (
@@ -24,32 +26,44 @@ export default ({ gateway, onVersionClick }) => {
       </div>
       <h2>Amazon Rekognition Custom Labels Demo</h2>
       <div className="powered">powered by Amazon Rekognition</div>
-      {Object.keys(projects).map((projectName, index) => (
-        <Card key={index}>
-          <Card.Header>{projectName}</Card.Header>
-          <Card.Body>
-            <ul>
-              {projects[projectName].map((version, index) => (
-                <li key={`v-${index}`}>
-                  {version.details.Status === "RUNNING" ? (
-                    <Button
-                      variant="link"
-                      onClick={() =>
-                        onVersionClick(version.details.ProjectVersionArn)
-                      }
-                    >
-                      {version.version}
-                    </Button>
-                  ) : (
-                    version.version
-                  )}{" "}
-                  ({version.details.Status})<br />
-                </li>
-              ))}
-            </ul>
-          </Card.Body>
-        </Card>
-      ))}
+      {errorDetails && (
+        <Alert variant="danger" style={{ marginTop: "30px" }}>
+          An error happened: {errorDetails}.{" "}
+          <a href={window.location.href}>Retry</a>.
+        </Alert>
+      )}
+      {!errorDetails && !projects && (
+        <Spinner animation="border" role="status" style={{ marginTop: "30px" }}>
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      )}
+      {projects &&
+        Object.keys(projects).map((projectName, index) => (
+          <Card key={index}>
+            <Card.Header>{projectName}</Card.Header>
+            <Card.Body>
+              <ul>
+                {projects[projectName].map((version, index) => (
+                  <li key={`v-${index}`}>
+                    {version.details.Status === "RUNNING" ? (
+                      <Button
+                        variant="link"
+                        onClick={() =>
+                          onVersionClick(version.details.ProjectVersionArn)
+                        }
+                      >
+                        {version.version}
+                      </Button>
+                    ) : (
+                      version.version
+                    )}{" "}
+                    ({version.details.Status})<br />
+                  </li>
+                ))}
+              </ul>
+            </Card.Body>
+          </Card>
+        ))}
     </div>
   );
 };
