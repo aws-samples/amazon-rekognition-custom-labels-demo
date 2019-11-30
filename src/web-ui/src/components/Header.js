2fab84296351dd451b3210ed9abe0d8a8dc8d36f
Matteo Figus <matteofigus@gmail.com>
2019-11-30 14:23:30 +0000

    Logo and header restyling

--- src/web-ui/src/components/Header.js
+++ src/web-ui/src/components/Header.js
@@ -3,26 +3,22 @@ import { Button, Container, Nav, Navbar } from "react-bootstrap";
 
 export default ({ onHelp, loadProjectList }) => (
   <Navbar
-    style={{ backgroundColor: "#000", marginBottom: "20px" }}
+    style={{ backgroundColor: "#232f3e", marginBottom: "20px" }}
     variant="dark"
   >
     <Container>
-      <Navbar.Brand>Amazon Rekognition Custom Labels Demo</Navbar.Brand>
+      <Navbar.Brand>
+        <div className="awslogo" />
+      </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Nav>
-          <Nav.Link
-            eventKey={1}
-            href="https://github.com/aws-samples/amazon-rekognition-custom-labels-demo"
-            target="_blank"
-          >
-            Fork me in github
-          </Nav.Link>
-        </Nav>
-        <Nav style={{ paddingTop: "8px" }} />
         <Nav className="ml-auto">
-          <Button onClick={loadProjectList} variant="primary">Projects</Button>
-          <Button onClick={onHelp} variant="light">Help</Button>
+          <Button onClick={loadProjectList} variant="primary">
+            Projects
+          </Button>
+          <Button onClick={onHelp} variant="light">
+            Help
+          </Button>
         </Nav>
       </Navbar.Collapse>
     </Container>
